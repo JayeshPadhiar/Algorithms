0ba1642780e5e5ed9448bacb78de8f9bd121fe06
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2021-05-31 11:09:31 +0530

    Bubble Sort

---
+++ algorithms/sorting/bubble sort.cpp
@@ -0,0 +1,42 @@
+/*
+ *		Bubble Sort
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void bubblesort(int arr[], int size)
+{
+	for(int i=0; i<size; i++)
+		for(int j=0; j<size-1; j++)
+			if(arr[j] > arr[j+1])
+				swap(&arr[j], &arr[j+1]);
+}
+
+int main()
+{
+	int size;
+
+	cout<<"Enter size: ";
+	cin>>size;
+
+	int arr[size];
+
+	for(int i=0; i<size; i++)
+		cin>>arr[i];
+
+	bubblesort(arr, size);
+
+	for(int i=0; i<size; i++)
+		cout<<arr[i]<<" ";
+
+	return 0;
+}
