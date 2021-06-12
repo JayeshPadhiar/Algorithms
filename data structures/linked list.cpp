0d046c3fd994a882c57c623901bfc863e4674c40
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2021-06-12 14:48:05 +0530

    Linked List Push

--- data structures/linked list.cpp
+++ data structures/linked list.cpp
@@ -44,6 +44,42 @@ int search(Node *node, int num)
 	return -1;
 }
 
+void push_front(Node **head, int num) // double pointer because head is not global
+{
+	Node *node = new Node(num);
+
+	node->next = *head;
+	*head = node;
+}
+
+void push_back(Node *head, int num)
+{
+	Node *node = new Node(num);
+
+	while(head->next)
+	{
+		head = head->next;
+	}
+	head->next = node;
+}
+
+void push_after(Node *node, int num, int data)
+{
+	while(node)
+	{
+		if(node->data == num)
+		{
+			Node *new_node = new Node(data);
+			new_node->next = node->next;
+			node->next = new_node;
+			return;
+		}
+		node = node->next;
+	}
+	cout<<"Number not found\n";
+}
+
+
 int main()
 {
 	Node *head = new Node(10);
