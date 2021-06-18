d8a21682dab2951f12b45df7338f2861b84bb01a
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2021-06-18 23:07:58 +0530

    Push Element in Stack

--- data structures/stack.cpp
+++ data structures/stack.cpp
@@ -19,10 +19,26 @@ class Node {
 		}
 };
 
+Node *push(Node **head, int num)
+{
+	Node *node = new Node(num);
+
+	node->next = *head;
+	*head = node;
+
+	return *head;
+}
+
 
 int main()
 {
 	Node *head = new Node(1);
+	push(&head, 2);
+	push(&head, 3);
+	push(&head, 4);
+	push(&head, 5);
+	push(&head, 6);
+	push(&head, 7);
 
 	return 0;
 }
