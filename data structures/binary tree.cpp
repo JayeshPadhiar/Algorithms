19c93dd3933a9ed566a91e7d94a46237c5cfbd17
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2021-06-23 16:29:35 +0530

    Binary Tree Init

--- data structures/binary tree.cpp
+++ data structures/binary tree.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 using namespace std;
 
+class Node {
+	public:
+		int data;
+		Node *left;
+		Node *right;
+
+		Node()
+		{
+			this->data = 0;
+			this->left = NULL;
+			this->right = NULL;
+		}
+
+		Node(int data)
+		{
+			this->data = data;
+			this->left = NULL;
+			this->right = NULL;
+		}
+};
+
 int main()
 {
-	//code
+	Node *root = new Node(1);
+
+	root->left = new Node(2);
+	root->right = new Node(3);
 
 	return 0;
 }
