7c2cf7c2b8bb0c46ca8796da0cfb1ec080b8580d
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2021-07-08 10:27:31 +0530

    Depth First Traversals

--- data structures/binary_search_tree.cpp
+++ data structures/binary_search_tree.cpp
@@ -22,12 +22,52 @@ class Node {
 		}
 };
 
+void preorder(Node *node)
+{
+	if(node != NULL)
+	{
+		cout<<node->data<<" ";
+		preorder(node->left);
+		preorder(node->right);
+	}
+}
+
+void inorder(Node *node)
+{
+	if(node != NULL)
+	{
+		inorder(node->left);
+		cout<<node->data<<" ";
+		inorder(node->right);
+	}
+}
+
+void postorder(Node *node)
+{
+	if(node != NULL)
+	{
+		postorder(node->left);
+		postorder(node->right);
+		cout<<node->data<<" ";
+	}
+}
+
 int main()
 {
-	Node *root = new Node(1);
-
+	Node* root = new Node(1);
 	root->left = new Node(2);
 	root->right = new Node(3);
+	root->left->left = new Node(4);
+	root->left->right = new Node(5);
+	
+	inorder(root);
+	cout<<endl;
+	
+    preorder(root);
+    cout<<endl;
+    
+    postorder(root);
+    cout<<endl;
 
 	return 0;
 }
